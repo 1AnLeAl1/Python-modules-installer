c5fe3bcc46d5f2f91fc1d52ade9bfe6900844f77
1AnLeAl1 <98709835+1AnLeAl1@users.noreply.github.com>
2022-04-24 21:15:32 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+    <script src="/script.js" defer></script>
+  </head>
+  <body>
+    gyhugsygyue
+    <form action="\post.php">
+      <input name="text">
+      <input type="submit">
+    </form>
+  </body>
+</html>
